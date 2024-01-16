128fd8b04b7db6b95fbb696c6f390c44c88050be
Nguyễn Văn Quang <137971471+QuangNguyeen@users.noreply.github.com>
2024-01-16 14:57:43 +0700

    Update CSE224_03.cpp

--- Final_Test/CSE224_03.cpp
+++ Final_Test/CSE224_03.cpp
@@ -1,135 +1,161 @@
 #include<iostream>
 using namespace std;
-class Nguoi{
-private:
-    char maNguoi, maDV;
-    char hoTen;
+
+class Nguoi {
+protected:
+    char maNguoi, hoTen, maDV;
     int namSinh;
     double heSoLuong;
+
 public:
-    Nguoi(){};
-    Nguoi(char id, char DV, char name, int year, double hs){
+    Nguoi() {};
+    Nguoi(char id, char name, char idDV, int year, double heso) {
         maNguoi = id;
-        maDV = DV;
         hoTen = name;
+        maDV = idDV;
         namSinh = year;
-        heSoLuong = hs;
+        heSoLuong = heso;
     }
-    virtual void input(){
+
+    virtual void input() {
         cout << "Nhap Ma Nguoi: "; cin >> maNguoi;
         cout << "Nhap Ho Ten: "; cin >> hoTen;
-        cout << "Nhap Ma Don Vi: "; cin >> maDV;
+        cout << "Nhap Ma DV: "; cin >> maDV;
         cout << "Nhap Nam Sinh: "; cin >> namSinh;
         cout << "Nhap He So Luong: "; cin >> heSoLuong;
     }
-    virtual void output(){
-        cout << "ID: " << maNguoi << "\t" << "ID Don Vi: " << maDV << endl;
-        cout << "HoTen: " << hoTen << "\t" << "NamSinh: " << namSinh << "\t" << "HSLuong: " << heSoLuong;
+
+    virtual void output() const {
+        cout << "Id: " << maNguoi << "\t" << "idDV: " << maDV << "\t" << "HoTen: " << hoTen << "\t" << "NamSinh: " << namSinh
+             << "\t" << "HeSoLuong: " << heSoLuong;
     }
+
+    virtual ~Nguoi() {}
 };
-class GiangVien : public Nguoi{
+
+class GiangVien : public Nguoi {
 private:
     char monGiangDay;
-    int soGio;
-    int soBaiBao;
+    int soGio, soBaiBao;
+
 public:
-    GiangVien(){};
-    GiangVien(char id, char DV, char name, int year, double hs, char subject, int hour, int news):Nguoi(id, DV,name,year,hs){
-        monGiangDay = subject;
-        soGio = hour;
+    GiangVien() {};
+    GiangVien(char id, char name, char idDV, int year, double heso, char monGD, int hour, int news)
+            : Nguoi(id, name, idDV, year, heso) {
+        monGiangDay = monGD;
         soBaiBao = news;
+        soGio = hour;
     }
-    double PhuCap(){
-        return 1200 * 0.25;
+
+    double Luong() const {
+        return soGio * 200 + 0.25 * 1200;
     }
-    double LuongThang(){
-        return PhuCap() + soGio * 200;
-    }
-    void input(){
+
+    void input() override {
         Nguoi::input();
-        cout << endl;
         cout << "Nhap Mon Giang Day: "; cin >> monGiangDay;
         cout << "Nhap So Gio: "; cin >> soGio;
         cout << "Nhap So Bai Bao: "; cin >> soBaiBao;
     }
-    void output(){
+
+    void output() const override {
         Nguoi::output();
-        cout << "Mon Giang Day: " << monGiangDay << "\t" << "SoGio: " << soGio << "\t" << "SoBaiBao: " << soBaiBao;
+        cout << "\tMon Giang Day: " << monGiangDay << "\t" << "SoGio: " << soGio << "\t" << "SoBaiBao: " << soBaiBao;
     }
 };
-class SinhVien : public Nguoi{
+
+class SinhVien : public Nguoi {
 private:
-    char Monhoc;
-    int soTinChi;
-    double diemQT;
+    char monHoc;
+    int soTC;
+    double DiemQT;
+
 public:
-    SinhVien(){};
-    SinhVien(char id, char DV, char name, int year, double hs, char subject, int Tinchi, double Diem):Nguoi(id, DV,name,year,hs){
-        Monhoc = subject;
-        soTinChi = Tinchi;
-        diemQT = Diem;
+    SinhVien() {};
+    SinhVien(char id, char name, char idDV, int year, double heso, char subject, int sotc, double diem)
+            : Nguoi(id, name, idDV, year, heso) {
+        monHoc = subject;
+        soTC = sotc;
+        DiemQT = diem;
     }
-    double getDiemQT(){
-        return diemQT;
+
+    double getDiem() const {
+        return DiemQT;
     }
-    void input(){
+
+    void input() override {
         Nguoi::input();
-        cout << endl;
-        cout << "Nhap Mon Hoc: "; cin >> Monhoc;
-        cout << "Nhap So Tin Chi: "; cin >> soTinChi;
-        cout << "Nhap Diem Qua Trinh: "; cin >> diemQT;
+        cout << "Nhap Mon Hoc: "; cin >> monHoc;
+        cout << "Nhap So Tin Chi: "; cin >> soTC;
+        cout << "Nhap Diem Qua Trinh: "; cin >> DiemQT;
     }
-    void output(){
+
+    void output() const override {
         Nguoi::output();
-        cout << endl;
-        cout << "Mon Hoc: " << Monhoc << "\t" << "SoTinChi: " << soTinChi << "\t" <<"DiemQT: "<< diemQT;
+        cout << "\tMonHoc: " << monHoc << "\t" << "SoTC: " << soTC << "\t" << "DiemQT: " << DiemQT;
     }
 };
-// selection sort
-void sort(Nguoi  **list, int n){
-    int i, max;
-    for(i = 0; i < n - 1; i++){
+
+void selectionSort(Nguoi** list, int n) {
+    int i, j, max;
+    for (i = 0; i < n - 1; i++) {
         max = i;
-        for(int j = 0; j < n; j++){
-            if(dynamic_cast<GiangVien*>(list[i])->LuongThang() > dynamic_cast<GiangVien*>(list[max])->LuongThang()){
+        for (j = i + 1; j < n; j++) {
+            if (dynamic_cast<GiangVien*>(list[i]) && dynamic_cast<GiangVien*>(list[j]) &&
+            static_cast<GiangVien *>(list[j])->Luong() > static_cast<GiangVien *>(list[max])->Luong())
                 max = j;
-            }
+
         }
-        if(i != max){
-            Nguoi *temp = list[i];
+        if (i != max) {
+            Nguoi* temp = list[i];
             list[i] = list[max];
             list[max] = temp;
         }
     }
 }
-int main(){
+
+int main() {
     int n;
-    do{
-        cout << "Nhap N nguoi ( 0 < N <= 100 ): "; cin >> n;
-    } while (n < 0 ||  n > 100);
-    Nguoi *list[n];
-    for(int i = 0; i < n; i++){
+    do {
+        cout << "Nhap N Nguoi ( 0 < N <= 100 ): "; cin >> n;
+    } while (n <= 0 || n > 100);
+
+    Nguoi* DanhSach[n];
+
+    for (int i = 0; i < n; i++) {
         int choice;
-        cout << "Nhap Lua Chon ( 0 la Giang Vien, 1 la Sinh Vien ): "; cin >> choice;
-        if(choice == 0){
-            list[i] = new GiangVien;
-            list[i]->input();
-        } else if (choice == 1){
-            list[i] = new SinhVien;
-            list[i]->input();
+        cout << "Nhap Lua Chon: ( 0 la SV, 1 la GV ): "; cin >> choice;
+        if (choice == 0) {
+            DanhSach[i] = new SinhVien;
+            DanhSach[i]->input();
+        } else if (choice == 1) {
+            DanhSach[i] = new GiangVien;
+            DanhSach[i]->input();
         }
     }
-    sort(list, n);
-    for(int i = 0; i < n; i++){
-        dynamic_cast<GiangVien*>(list[i])->output();
-        cout << endl;
-    }
-    cout << "\nDanh Sach Sinh Vien Cam Thi: \n";
-    for(int i = 0; i < n; i++){
-        if(dynamic_cast<SinhVien*>(list[i])->getDiemQT() < 4){
-            list[i]->output();
+
+    selectionSort(DanhSach, n);
+
+    cout << "\nDanh Sach Giang Vien sau khi sap xep: \n";
+    for (int i = 0; i < n; i++) {
+        if (dynamic_cast<GiangVien*>(DanhSach[i])) {
+            DanhSach[i]->output();
             cout << endl;
         }
     }
+
+    cout << "\nDanh Sach Sinh Vien Cam Thi: \n";
+    for (int i = 0; i < n; i++) {
+        if (dynamic_cast<SinhVien*>(DanhSach[i])->getDiem() < 4) {
+            DanhSach[i]->output();
+            cout << endl;
+        }
+    }
+
+    // Free allocated memory
+    for (int i = 0; i < n; i++) {
+        delete DanhSach[i];
+    }
+
     return 0;
 }
